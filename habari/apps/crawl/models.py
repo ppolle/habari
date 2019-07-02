c2c09f234e4104f59ada02875d6f40fdf50710e7
ppolle <peter.m.polle@gmail.com>
2019-07-03 01:48:47 +0300

    this commit will update the initial models

--- habari/apps/crawl/models.py
+++ habari/apps/crawl/models.py
@@ -1,3 +1,15 @@
+from autoslug import AutoSlugField
 from django.db import models
 
 # Create your models here.
+class Article(models.Model):
+	'''Model that saves crawled articles'''
+	title = models.CharField(max_length=500)
+	article_url = models.URLField(max_length=500)
+	article_image_url = models.URLField(max_length=500)
+	summary = models.CharField(max_length=3000)
+	slug = AutoSlugField(blank=False, populate_from='title', unique=True)
+	timestamp = models.DateTimeField(auto_now_add=True)
+
+	def __str__(self):
+		return self.title
