987c173b22e0c2fccdfff04e85c7e2fef85007fa
ppolle <peter.m.polle@gmail.com>
2019-07-19 02:41:44 +0300

    this commit will update custom management crawl  commands

--- habari/apps/crawl/crawler.py
+++ habari/apps/crawl/crawler.py
@@ -7,57 +7,59 @@ class DNCrawler:
 		self.url = 'https://www.nation.co.ke/'
 
 	def get_top_stories(self):
+		print('Getting top stories')
 		top_stories = requests.get(self.url)
 		story_links = []
 		if top_stories.status_code == 200:
 			soup = soup = BeautifulSoup(top_stories.content, 'html.parser')
 			teaser = soup.select('.story-teaser a')
 			for t in teaser:
-				while t not in story_links:
+				if t not in story_links:
 					story_links.append(
 					    {'url': self.make_relative_links_absolute(t.get('href'))})
-
-				print(self.make_relative_links_absolute(t.get('href')))
 		return story_links
 			# print(teaser)
 			# print(soup.prettify())
 
 	def make_relative_links_absolute(self, link):
+		print('Sanitizing '+ str(link))
 		import re
-        try:
-            from urlparse import urljoin
-        except ImportError:
-            from urllib.parse import urljoin
+		try:
+			from urlparse import urljoin
+		except ImportError:
+			from urllib.parse import urljoin
 
-        if not link.starts_with(self.url):
+		if not link.startswith(self.url):
 			link = urljoin(self.url, link)
-        
-        return link
 
-    def get_story_details(self, link):
-    	story= request.get(link)
-    	if story.status_code == 200:
-    		soup = BeautifulSoup(story.content, 'html.parser')
-    		url = link
-    		image_url = soup.select('.story-view header img')
-    		title = soup.select('.story-view header h2').get_text()
-    		publication_date = soup.select('.story-view header h6').get_text()
-    		author = soup.select('.story-view .author strong').get_text()
-    		return {'article_url':url,
-    				'article_url':image_url,
-    				'article_title':title,
-    				'publication_date':publication_date,
-    				'author':author}
+		return link
 
-    def update_top_stories(self):
-    	top_articles = self.get_top_stories()
-    	article_info = []
-    	for article in top_articles:
-    		article_info.append(article)
+	def get_story_details(self, link):
+		story= requests.get(link)
+		if story.status_code == 200:
+			soup = BeautifulSoup(story.content, 'html.parser')
+			url = link
+			image_url = self.make_relative_links_absolute(soup.select('.story-view header img')[0].get('src'))
+			title = [t.get_text() for t in soup.select('.story-view header h2')][0]
+			publication_date = [p.get_text() for p in soup.select('.story-view header h6')][0]
+			author = [a.get_text() for a in soup.select('.story-view .author strong')][0]
+		else:
+			print('Getting stuff failed')
 
-    	return article_info
+		return {'article_url':url,
+    			'image_url':image_url,
+    			'article_title':title,
+    			'publication_date':publication_date,
+    			'author':author}
 
+	def update_top_stories(self):
+		top_articles = self.get_top_stories()
+		article_info = []
+		for article in top_articles:
+			print('Updating top stories')
+			article_info.append(self.get_story_details(article['url']))
 
+		return article_info
 
 crawler = DNCrawler()
-crawl = print(crawler.get_story_details('https://www.nation.co.ke/news/PSC-to-hire-gardeners-cooks-for-Kibaki-and-Moi/1056-5201290-p2wuou/index.html'))
+crawl = print(crawler.update_top_stories())
---
+++ habari/apps/crawl/management/commands/crawl.py
@@ -0,0 +1,14 @@
+from django.core.management.base import BaseCommand
+from django.utils import timezone
+from habari.apps.crawl.crawler import DNCrawler
+
+class Command(BaseCommand):
+    help = 'Displays current time'
+
+    def handle(self, *args, **kwargs):
+    	crawler = DNCrawler()
+    	crawl = crawler.update_top_stories()
+    	# time = timezone.now().strftime('%X')
+    	# self.stdout.write("It's now %s" % time)
+    	for c in crawl:
+    		self.stdout.write(c)
