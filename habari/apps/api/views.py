f3bfc00b7ce31d125f75583147099a12d320c9a6
ppolle <peter.m.polle@gmail.com>
2019-11-02 13:34:51 +0300

    this commit will limit API list queries to articles created in the last 1 day

--- habari/apps/api/views.py
+++ habari/apps/api/views.py
@@ -12,6 +12,12 @@ class ArticleList(generics.ListAPIView):
 	"""
 	Retrieve a list of news articles from different sources
 	"""
-	queryset = Article.objects.all()
 	serializer_class = ArticleSerializer
 	filterset_class = ArticleFilter
+
+	def get_queryset(self):
+		from datetime import datetime, timedelta
+
+		time_threshold = datetime.today() - timedelta(days=1)
+		results = Article.objects.filter(publication_date__gte=time_threshold)
+		return results
