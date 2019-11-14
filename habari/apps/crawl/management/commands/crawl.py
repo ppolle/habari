f85f60d30e1a28de9fe7f1b418e2f0095b8f5a78
ppolle <peter.m.polle@gmail.com>
2019-11-14 23:31:13 +0300

    this commit will update EA crawl management commands

--- habari/apps/crawl/management/commands/crawl.py
+++ habari/apps/crawl/management/commands/crawl.py
@@ -2,36 +2,41 @@ from django.core.management.base import BaseCommand
 from django.utils import timezone
 from habari.apps.crawl import crawler
 
+
 class Command(BaseCommand):
     help = 'Crawl News Sources for New Articles'
 
     def add_arguments(self, parser):
-    	parser.add_argument('sources', type=str, help='Indicated the news sources to be crawled')
+        parser.add_argument('sources', type=str,
+                            help='Indicated the news sources to be crawled')
 
     def handle(self, *args, **kwargs):
-    	source = kwargs['sources']
-
-    	if source == 'BD':
-    		bd_crawler = crawler.BDCrawler()
-    		crawl = bd_crawler.update_top_stories()
-    		self.stdout.write(self.style.SUCCESS('Succesfully updated Business Daily Latest Articles'))
-
-    	if source == 'DN':
-    		dn_crawler = crawler.DNCrawler()
-    		crawl = dn_crawler.update_top_stories()
-    		self.stdout.write(self.style.SUCCESS('Succesfully updated Daily Nation Latest Articles.'))
-
+        source = kwargs['sources']
+        
         if source == 'EA':
             ea_crawler = crawler.EACrawler()
             crawl = ea_crawler.update_top_stories()
-            self.stdout.write(self.style.SUCCESS('Succesfully updated The east African Latest Articles.'))
+            self.stdout.write(self.style.SUCCESS(
+                'Succesfully updated The East African Latest Articles.'))
 
-    	if source == 'all':
-    		dn_crawler = crawler.DNCrawler()
-    		bd_crawler = crawler.BDCrawler()
+        if source == 'BD':
+            bd_crawler = crawler.BDCrawler()
+            crawl = bd_crawler.update_top_stories()
+            self.stdout.write(self.style.SUCCESS(
+                'Succesfully updated Business Daily Latest Articles'))
 
-    		dn_crawl = dn_crawler.update_top_stories()
-    		bd_crawl = bd_crawler.update_top_stories()
+        if source == 'DN':
+            dn_crawler = crawler.DNCrawler()
+            crawl = dn_crawler.update_top_stories()
+            self.stdout.write(self.style.SUCCESS(
+                'Succesfully updated Daily Nation Latest Articles.'))
 
-    		self.stdout.write(self.style.SUCCESS('Succesfully updated Latest Articles'))
+        if source == 'all':
+            dn_crawler = crawler.DNCrawler()
+            bd_crawler = crawler.BDCrawler()
 
+            dn_crawl = dn_crawler.update_top_stories()
+            bd_crawl = bd_crawler.update_top_stories()
+
+            self.stdout.write(self.style.SUCCESS(
+                'Succesfully updated Latest Articles'))
