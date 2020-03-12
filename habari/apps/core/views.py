551155409afc1f60313f0360f4a7c109914856b6
ppolle <peter.m.polle@gmail.com>
2020-03-12 16:14:42 +0300

    this commit will update templating

--- habari/apps/core/views.py
+++ habari/apps/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from habari.apps.crawl.models import Article
 
 # Create your views here.
+
+def index(request):
+	articles = Article.objects.all()
+	return render(request, 'core/index.html', {'articles':articles})
