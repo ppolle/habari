ee7dce14404f0ad54d6878ee125a3166b0f5e621
ppolle <peter.m.polle@gmail.com>
2020-04-11 13:54:05 +0300

    this commit will debug urls

--- habari/apps/core/views.py
+++ habari/apps/core/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-from habari.apps.crawl.models import Article
+from habari.apps.crawl.models import Article, NewsSource
 
 # Create your views here.
 
 def index(request):
 	articles = Article.objects.all()
 	return render(request, 'core/index.html', {'articles':articles})
+
+def status(request):
+	sources = NewsSource.objects.all()
+	return render(request, 'core/status.html', {'sources':sources})
