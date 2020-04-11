235ed9e7cb65fb8c3d34df5e47eb08b5655e5aa2
ppolle <peter.m.polle@gmail.com>
2020-04-12 00:38:24 +0300

    this commit will update front end templating

--- habari/apps/core/views.py
+++ habari/apps/core/views.py
@@ -4,9 +4,9 @@ from habari.apps.crawl.models import Article, NewsSource
 # Create your views here.
 
 def index(request):
-	articles = Article.objects.all()
-	return render(request, 'core/index.html', {'articles':articles})
+	sources = NewsSource.objects.all()
+	return render(request, 'core/index.html', {'sources':sources})
 
 def status(request):
-	sources = NewsSource.objects.all()
+	sources = NewsSource.objects.all().order_by('pk')
 	return render(request, 'core/status.html', {'sources':sources})
