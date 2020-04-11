e0884689721a96a5b2a3f8ecc347323ebae902f8
ppolle <peter.m.polle@gmail.com>
2020-04-11 12:17:03 +0300

    this commit will make updates to the context_processos

--- habari/apps/core/context_processors.py
+++ habari/apps/core/context_processors.py
@@ -1,5 +1,5 @@
 from habari.apps.crawl.models import NewsSource
 
-def get_source_names(request):
+def get_source_names():
 	sources = NewsSource.objects.values_list('name', flat=True)
 	return {'sources':sources}
