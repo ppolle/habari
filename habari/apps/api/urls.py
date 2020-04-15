08c5bc418925b2dc40ca11af208f1c0c13871959
ppolle <peter.m.polle@gmail.com>
2020-04-16 00:20:14 +0300

    this commit will update urls code to make them work

--- habari/apps/api/urls.py
+++ habari/apps/api/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import url
 from habari.apps.api.views import ArticleList
 
 urlpatterns = [
-	url(r'news/$', ArticleList.as_view()),
+	url(r'^news/$', ArticleList.as_view()),
 
 ]
--- habari/urls.py
+++ habari/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'', include('habari.apps.core.urls')),
     url(r'^api/v1/', include('habari.apps.api.urls')),
+    url(r'', include('habari.apps.core.urls')),
+    
 ]
