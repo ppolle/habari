243d26f185e9fcf0869511d2c87afe29dd3274ad
ppolle <peter.m.polle@gmail.com>
2020-04-16 20:24:55 +0300

    this commit will update functionality to make all news sources available from the bottom nav

--- habari/apps/core/context_processors.py
+++ habari/apps/core/context_processors.py
@@ -1,5 +1,5 @@
 from habari.apps.crawl.models import NewsSource
 
 def get_source_names(request):
-	sources = NewsSource.objects.values_list('name', flat=True)
+	sources = NewsSource.objects.values('name', 'slug')
 	return {'sources':sources}
