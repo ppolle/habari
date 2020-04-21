a6f24b2aff87a822bbffaadc071c5c977b074d0f
ppolle <peter.m.polle@gmail.com>
2020-04-21 20:51:34 +0300

    this commit will change core filter to core_filter

--- habari/apps/core/templatetags/core_filter.py
+++ habari/apps/core/templatetags/core_filter.py
@@ -9,7 +9,7 @@ def author_slug(value):
 
 @register.filter
 def author_capitalize(value):
-	cap_list = ['afp','reuters','bbc','a.p','xinhua']
+	cap_list = ['afp','reuters','bbc','a.p','xinhua','mirror']
 	val = value.strip().lower()
 	if val in cap_list:
 		return value.upper()
