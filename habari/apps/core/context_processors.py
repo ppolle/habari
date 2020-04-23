af0b67543036d245e2cc4aef506fad1e5ddc0328
ppolle <peter.m.polle@gmail.com>
2020-04-23 22:52:14 +0300

    this commit will update how bottom nav is rendered

--- habari/apps/core/context_processors.py
+++ habari/apps/core/context_processors.py
@@ -2,4 +2,4 @@ from habari.apps.crawl.models import NewsSource
 
 def get_source_names(request):
 	sources = NewsSource.objects.values('name', 'slug').order_by('pk')
-	return {'sources':sources}
+	return {'news_sources':sources}
