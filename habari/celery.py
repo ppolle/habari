87bb0fa828395a711ca5cb438acdf1453c3aa004
ppolle <peter.m.polle@gmail.com>
2020-04-30 18:03:54 +0300

    this  commit will update celery beat task queues and change the location of celery beat setting to thesetting file

--- habari/celery.py
+++ habari/celery.py
@@ -1,7 +1,6 @@
 from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'habari.settings')
 
@@ -13,26 +12,3 @@ app.autodiscover_tasks()
 def debug_task(self):
     print('Request: {0!r}'.format(self.request))
 
-app.conf.beat_schedule = {
-    'frequent_crawlers':{
-    'task':'frequent_crawlers',
-    'schedule':crontab(minute=0,hour='*/4'),
-    },
-    'non_frequent_crawlers':{
-    'task':'non_frequent_crawlers',
-    'schedule':crontab(minute=0,hour='*/6')
-    },
-    'bd_crawler':{
-    'task':'bd_crawler',
-    'schedule':crontab(minute=0,hour='*/5',day_of_week='1-5')
-    },
-    'retry_failed_crawls':{
-    'task': 'retry_failed_crawls',
-    'schedule': crontab(minute='*/30')
-    },
-    'sanitize_sm_author_field':{
-    'task': 'sanitize_sm_author_lists_with_empty_strings',
-    'schedule': crontab(minute='*/5')
-    }
-
-}
