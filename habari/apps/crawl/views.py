85b9b2765804eefabd285ffaa509f8d621eb5cba
ppolle <peter.m.polle@gmail.com>
2020-05-07 14:17:23 +0300

    this commit will update remove junk code from crawl views file

--- habari/apps/crawl/views.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.generics import ListAPiView
-from .serializers import ArticleSerializer
-from habari.apps.crawl.models import Article
-
-# Create your views here.
-
-class ArticleList(ListApiView):
-	queryset = Article.objects.all()
-	serializer_class = ArticleSerializer
