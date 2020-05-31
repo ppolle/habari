9ff66aa902a7d771fffe929ae5f32b48b1bbf0c8
ppolle <peter.m.polle@gmail.com>
2020-05-31 20:37:38 +0300

    this commit will update distribution of settings files

--- habari/settings/prod.py
+++ habari/settings/prod.py
@@ -4,5 +4,38 @@ from .base import *
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 DEBUG = False
-
+CSRF_COOKIE_SECURE = True
+SESSION_COOKIE_SECURE = True
 SECRET_KEY = config('SECRET_KEY')
+
+# ==============================================================================
+# Logging settings
+# ==============================================================================
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'formatters': {
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(message)s'
+        },
+        'simple': {
+            'format': '%(message)s'
+        },
+    },
+
+    'handlers': {
+        'console': {
+            'level': 'INFO',
+            'class': 'logging.StreamHandler',
+            'formatter': 'simple'
+        }
+
+    },
+    'loggers': {
+        '': {
+            'handlers': ['console'],
+            'level': 'INFO',
+        }
+    }
+}
