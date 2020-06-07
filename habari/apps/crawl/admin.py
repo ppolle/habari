e2eb2c700d00d8350fce7dbc97dee2ac5184af9d
ppolle <peter.m.polle@gmail.com>
2020-06-07 18:15:14 +0300

    this commit will update models in admin section

--- habari/apps/crawl/admin.py
+++ habari/apps/crawl/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Article
+from .models import Article, NewsSource, Crawl
 # Register your models here.
 admin.site.register(Article)
+admin.site.register(NewsSource)
+admin.site.register(Crawl)
